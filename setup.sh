620e8bf76c0f8aa7feb8e2f40c8673a19d40ed94
Arjun Kochhar <akochhar@omnissa.com>
2024-10-04 16:37:27 -0400

    Provide option to configure grafana, loki and prometheus

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@ if [ $# -ne 1 ]; then
    echo "Run setup.sh with: 
 	0: if all components need to run
 	1: if everything except syslog needs to be deployed
-	2: if everthing except syslog and grafana needs to be deployed "
+	2: if everthing except syslog and grafana needs to be deployed
+     glp: if Grafana, Loki and Prometheus needs to be deployed
+     clean: clean up all containers"
    exit 1
 fi
 
@@ -15,4 +17,8 @@ case "$1" in
        docker-compose up --build --force-recreate -d influxdb telegraf grafana loki promtail; shift;;
   "2")  
        docker-compose up --build --force-recreate -d influxdb telegraf loki promtail; shift;;
+  "glp")
+       docker-compose -f docker-compose-glp.yml up --build --force-recreate -d; shift;;  
+  "clean")  
+       docker-compose down --remove-orphans; shift;; 
 esac
