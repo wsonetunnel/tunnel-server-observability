dd6b18fd3d84a5c69f4b495d32be1768b44c5650
Arjun Kochhar <arjun.ice@gmail.com>
2024-10-17 20:00:05 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,22 +2,22 @@
 
 if [ $# -ne 1 ]; then
    echo "Run setup.sh with: 
-	0: if all components need to run
-	1: if everything except syslog needs to be deployed
-	2: if everthing except syslog and grafana needs to be deployed
-     glp: if Grafana, Loki and Prometheus needs to be deployed
-     clean: clean up all containers"
+	tunall: if all tunnel components need to run
+	tunall-syslog: if everything except syslog needs to be deployed
+	tunall-sys-gra: if everthing except syslog and grafana needs to be deployed
+        ws1all: if all ws1 components like Grafana, Loki and Prometheus needs to be deployed
+        clean: clean up all containers"
    exit 1
 fi
 
 case "$1" in
-  "0")
+  "tunall")
        docker-compose up --build --force-recreate -d; shift;;
-  "1") 
+  "tunall-sys") 
        docker-compose up --build --force-recreate -d influxdb telegraf grafana loki promtail; shift;;
-  "2")  
+  "tunall-sys-gra")  
        docker-compose up --build --force-recreate -d influxdb telegraf loki promtail; shift;;
-  "glp")
+  "ws1all")
        docker-compose -f docker-compose-glp.yml up --build --force-recreate -d; shift;;  
   "clean")  
        docker-compose down --remove-orphans; shift;; 
